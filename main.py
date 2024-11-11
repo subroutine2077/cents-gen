579420aaa2727de09b0643494c243555e031e8de
subroutine2077 <subroutine2077@gmail.com>
2024-11-11 13:16:34 +0000

    Some overhaul

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import openpyxl
 
+# cents.xlsx is pre-downloaded
 df = pd.read_excel('cents.xlsx')
 
+# Filtering for columns
 columns_of_interest = [
     'Number 7000',
     'Vote #2 Coloration: Verdigris',
@@ -12,9 +15,9 @@ columns_of_interest = [
     'Inscription ID',
     'Image URL'
 ]
-
 filtered = df[columns_of_interest]
 
+# Rename columns for readability
 filtered.rename(columns={
     'Number 7000': 'Number',
     'Vote #2 Coloration: Verdigris': 'Verdigris',
@@ -24,6 +27,7 @@ filtered.rename(columns={
     'Vote #2\nColoration: Obsidian': 'Obsidian'
 }, inplace=True)
 
+# Prepare for sheets generation
 sheets_names = [
     'Verdigris',
     'Red',
@@ -34,6 +38,7 @@ sheets_names = [
 
 sheets = {}
 
+# Loop generation of all notables
 for sheet_name in sheets_names:
     df_tmp = filtered[[
         'Number',
@@ -42,21 +47,29 @@ for sheet_name in sheets_names:
         'Image URL'
     ]]
 
-    df_tmp = df_tmp[df_tmp[sheet_name] == 'Notable']
+    # Filter for notables
+    notables = df_tmp[df_tmp[sheet_name] == 'Notable']
+    # Rename column name
+    notables.rename(columns={'Image URL': 'IMAGE'}, inplace=True)
 
-    # IMAGE URL display not working
-    '''
-    9988    =IMAGE(0       https://rutherfordchang.com/cen...
-    9990    =IMAGE(0       https://rutherfordchang.com/cen...
-    9991    =IMAGE(0       https://rutherfordchang.com/cen...
-    9993    =IMAGE(0       https://rutherfordchang.com/cen...
-    9997    =IMAGE(0       https://rutherfordchang.com/cen...
-    '''
-    df_tmp['Image URL'] = "=IMAGE({0})".format(df_tmp['Image URL'])
-    df_tmp = df_tmp.rename(columns={'Image URL': 'IMAGE'})
-    sheets[sheet_name] = df_tmp['IMAGE']
+    # Leave only IMAGE column in place
+    image_list = notables['IMAGE'].tolist()
+    
+    # Open new workbook with openpyxl
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = sheet_name
 
-    print(sheets[sheet_name].tail())
+    # Loop through list and fill column
+    for row_num, item in enumerate(image_list, start=1):
+        google_sheet_img_formula = f"=IMAGE(item)"
+        ws.cell(row=row_num, column=1, value=item)
+    
+    # Save to file for testing
+    # Need to view files, unable to view in VSCode server
+    wb.save(f"notables/{sheet_name}.xlsx")
 
+# Save all notables to files
+'''
 for sheet_name, sheet in sheets.items():
-    sheet.to_csv(f"{sheet_name}.csv", index=False)
+    sheet.to_csv(f"notables/{sheet_name}.csv", index=False)'''
