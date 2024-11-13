19c039724978d9bd141695914cd3d858497a3aa2
subroutine2077 <subroutine2077@gmail.com>
2024-11-13 15:26:54 +0000

    Major update

---
+++ README.md
@@ -0,0 +1,17 @@
+### Setup
+
+Download the full review sheet as `cents.xlsx`
+
+Install packages
+
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+Run
+
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import pandas as pd
 import openpyxl
+import zipfile
+import os
+
+def compress_folder(folder_path, zip_file_name):
+  """
+  Compresses a folder into a ZIP archive.
+
+  Args:
+    folder_path: The path to the folder to compress.
+    zip_file_name: The name of the ZIP file to create.
+  """
+
+  with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+    for root, dirs, files in os.walk(folder_path):
+      for file in files:
+        zip_file.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), folder_path))
 
 # cents.xlsx is pre-downloaded
 df = pd.read_excel('cents.xlsx')
@@ -47,29 +63,79 @@ for sheet_name in sheets_names:
         'Image URL'
     ]]
 
-    # Filter for notables
+    # Filter for notables and fulls
     notables = df_tmp[df_tmp[sheet_name] == 'Notable']
+    fulls = df_tmp[df_tmp[sheet_name] == 'Full']
     # Rename column name
     notables.rename(columns={'Image URL': 'IMAGE'}, inplace=True)
+    fulls.rename(columns={'Image URL': 'IMAGE'}, inplace=True)
 
     # Leave only IMAGE column in place
-    image_list = notables['IMAGE'].tolist()
-    
-    # Open new workbook with openpyxl
-    wb = openpyxl.Workbook()
-    ws = wb.active
-    ws.title = sheet_name
+    notable_records = notables.to_dict('records')
+    full_records = fulls.to_dict('records')
 
-    # Loop through list and fill column
-    for row_num, item in enumerate(image_list, start=1):
-        google_sheet_img_formula = f"=IMAGE(item)"
-        ws.cell(row=row_num, column=1, value=item)
     
-    # Save to file for testing
-    # Need to view files, unable to view in VSCode server
-    wb.save(f"notables/{sheet_name}.xlsx")
+    # Loop through records to create xlsx file
 
-# Save all notables to files
-'''
-for sheet_name, sheet in sheets.items():
-    sheet.to_csv(f"notables/{sheet_name}.csv", index=False)'''
+    # Create a new workbook and worksheet
+    workbook = openpyxl.Workbook()
+    worksheet = workbook.active
+    
+    # Iterate through the data, inserting into the worksheet
+
+    # Notables
+    row = 1
+    col = 1
+    for item in notable_records:
+        worksheet.cell(row=row, column=col).value = item['Number']
+        worksheet.cell(row=row+1, column=col).value = '=IMAGE("{0}")'.format(item['IMAGE'])
+        worksheet.cell(row=row+2, column=col).value = item['Inscription ID']
+        worksheet.cell(row=row+3, column=col).value = 1
+
+        col += 1
+        if col > 6:
+            row += 5
+            col = 1
+    
+    # Adjust size for viewability
+    for scol in 'ABCDEF':
+        worksheet.column_dimensions[scol].width = 35
+    for srow in range(1, row+1):
+        if srow % 5 == 2:
+            worksheet.row_dimensions[srow].height = 200
+        if srow % 5 == 0:
+            worksheet.row_dimensions[srow].height = 100
+
+    # Save the workbook
+    workbook.save(f'notables/{sheet_name}_notables.xlsx')
+
+
+    # Fulls
+    row = 1
+    col = 1
+    for item in full_records:
+        worksheet.cell(row=row, column=col).value = item['Number']
+        worksheet.cell(row=row+1, column=col).value = '=IMAGE("{0}")'.format(item['IMAGE'])
+        worksheet.cell(row=row+2, column=col).value = item['Inscription ID']
+        worksheet.cell(row=row+3, column=col).value = 1
+
+        col += 1
+        if col > 6:
+            row += 5
+            col = 1
+    
+    # Adjust size for viewability
+    for scol in 'ABCDEF':
+        worksheet.column_dimensions[scol].width = 35
+    for srow in range(1, row+1):
+        if srow % 5 == 2:
+            worksheet.row_dimensions[srow].height = 200
+        if srow % 5 == 0:
+            worksheet.row_dimensions[srow].height = 100
+
+    # Save the workbook
+    workbook.save(f'fulls/{sheet_name}_fulls.xlsx')
+
+# Compress both notables and fulls into zip archive
+compress_folder("notables", "notables.zip")
+compress_folder("fulls", "fulls.zip")
